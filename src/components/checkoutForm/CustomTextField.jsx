0f1aa604a67ef1932d3133743f08a0281718bbb4
Wilkenson Hilarion <wilkenson@Wilkensons-MBP.home>
2021-12-04 20:21:55 -0500

    suctomfield update

--- src/components/checkoutForm/CustomTextField.jsx
+++ src/components/checkoutForm/CustomTextField.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TextField, Grid } from "@material-ui/core";
 import { useFormContext, Controller } from "react-hook-form";
 
-const FormInput = ({ name, label}) => {
+const FormInput = ({ name, label }) => {
   const { control } = useFormContext();
   return (
     <Grid item xs={12} sm={6}>
@@ -10,11 +10,11 @@ const FormInput = ({ name, label}) => {
         control={control}
         defaultValue=""
         as={TextField}
-        name={name}         
-        render={({field}) => (
+        name={name}
+        render={({ field }) => (
           <TextField {...field} name={name} label={label} fullWidth required />
         )}
-      />      
+      />
     </Grid>
   );
 };
