372c5daf8d9fd21d20d9da82355dffd33d9cc841
Wilkenson Hilarion <wilkenson@Wilkensons-MBP.home>
2021-12-08 08:00:09 -0500

    product card styled

--- src/components/product/styles.js
+++ src/components/product/styles.js
@@ -3,29 +3,32 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles((theme) => ({
   root: {
-    marginTop: "15px",
-    maxWidth: "100%",
+    margin: "15px auto",
+    maxWidth: "80%",
   },
   main: {
-    margin:"10px"
+    margin: "10px",
   },
   media: {
     height: 0,
     paddingTop: "56.25%",
+    margin: "10px auto",
+    width: "50%",    
   },
   cardActions: {
     display: "flex",
-    justifyContent: "flex-end",
+    justifyContent: "flex-end",    
   },
   cardContent: {
     display: "flex",
-    justifyContent: "space-between",
+    justifyContent: "space-between",    
   },
   //
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
+    padding: theme.spacing(1),
+     
   },
 }));
