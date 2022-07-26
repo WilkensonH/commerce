e1130d5230578c9c17d2f02644ab48c066e8fd1d
wilkenson <tcbwilkenson@gmail.com>
2022-07-26 14:35:08 -0400

    about mod

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,70 +14,9 @@ const About = () => {
         <img src="asset/ezcom.png" alt="" />
         <div className="prof-divider">
           <div className="border">
-            <Grid className="display-grid">
-              <Card>
-                <CardMedia
-                  component="img"
-                  alt="profile picture"
-                  height="500"
-                  image="asset/com.jpg"
-                />
-                <CardContent className="content-card">
-                  <Typography variant="body2" color="text.secondary">
-                    Full Stack Web and Mobile Developer, Coding Bootcamp
-                    Graduate and <strong>Enrolled</strong> undergraduate
-                    Computer Science degree. Love coding would like to have the
-                    opportunity to sharpened my skills.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
+            <h1>Under Construction</h1>
           </div>
         </div>
-        <div className="profI-divider">
-          <div className="border">
-            <Grid className="display-grid">
-              <Card>
-                <CardMedia
-                  component="img"
-                  alt="profile picture"
-                  height="500"
-                  image="asset/web.jpg"
-                />
-                <CardContent className="content-card">
-                  <Typography variant="body2" color="text.secondary">
-                    Full Stack Web and Mobile Developer, Coding Bootcamp
-                    Graduate and <strong>Enrolled</strong> undergraduate
-                    Computer Science degree. Love coding would like to have the
-                    opportunity to sharpened my skills.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </div>
-        </div>
-        <div className="profII-divider">
-          <div className="border">
-            <Grid className="display-grid">
-              <Card>
-                <CardMedia
-                  component="img"
-                  alt="profile picture"
-                  height="500"
-                  image="asset/multicom.jpg"
-                />
-                <CardContent className="content-card">
-                  <Typography variant="body2" color="text.secondary">
-                    Full Stack Web and Mobile Developer, Coding Bootcamp
-                    Graduate and <strong>Enrolled</strong> undergraduate
-                    Computer Science degree. Love coding would like to have the
-                    opportunity to sharpened my skills.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </div>
-        </div>    
       </div>
     </main>
   );
